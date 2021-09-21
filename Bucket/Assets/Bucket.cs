0e4cef2783f80d1ffe01dfad4684331eeffa5575
Simao-Soares <44780464+Simao-Soares@users.noreply.github.com>
2021-09-21 18:11:47 +0100

    First Commit with meta files
    Developing UI

---
+++ Bucket/Assets/Bucket.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bucket : MonoBehaviour
+{
+
+
+
+    void OnCollisionEnter(Collision collisionInfo) {
+
+        if (collisionInfo.collider.tag == "Ball")
+        {
+            Debug.Log("Cesto!");
+        }
+
+
+        
+    }
+}
