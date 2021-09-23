a74560210b54b83c660dc1424dd255fdcb85c016
Simao-Soares <44780464+Simao-Soares@users.noreply.github.com>
2021-09-23 16:27:46 +0100

    Bucket complete
    now trying to implement leap motion control and maybe new features

--- Bucket/Assets/Scripts/GameManager.cs
+++ Bucket/Assets/Scripts/GameManager.cs
@@ -6,13 +6,55 @@ public class GameManager : MonoBehaviour
     private int playerScore;
     public Text playerScoreText;
 
-    public void PlayerScores(){
+    private int NGreenBalls;
+    private int NYellowBalls;
+    private int NGreyBalls;
+
+    public Text NGreenBallsText;
+    public Text NYellowBallsText;
+    public Text NGreyBallsText;
+    
+    private GameObject GreenBall;
+    private GameObject YellowBall;
+    private GameObject GreyBall;
+
+    //increment
+
+
+    public void PlayerScores5(){
         playerScore = playerScore + 5;
         this.playerScoreText.text = playerScore.ToString();
+
+        NGreenBalls += 1;
+        this.NGreenBallsText.text = NGreenBalls.ToString();
+
         Debug.Log("+5 pontos");
-        //ResetPos?
+        GreenBall = GameObject.Find("Ball1");
+        GreenBall.GetComponent<BallLogic>().ResetPos();
+
+    }
+        
+    public void PlayerScores10(){
+        playerScore = playerScore + 10;
+        this.playerScoreText.text = playerScore.ToString();
+
+        NYellowBalls += 1;
+        this.NYellowBallsText.text = NYellowBalls.ToString();
+
+        Debug.Log("+10 pontos");
+        YellowBall = GameObject.Find("Ball2");
+        YellowBall.GetComponent<BallLogic>().ResetPos();
     }
 
+    public void PlayerScores1(){
+        playerScore = playerScore + 1;
+        this.playerScoreText.text = playerScore.ToString();
 
+        NGreyBalls += 1;
+        this.NGreyBallsText.text = NGreyBalls.ToString();
 
+        Debug.Log("+1 pontos");
+        GreyBall = GameObject.Find("Ball3");
+        GreyBall.GetComponent<BallLogic>().ResetPos();
+    }
 }
